eee503b9ae44fef3bc35985a73fd9b3c52029bec
Dave Daly <havingabath@gmail.com>
2011-06-30 18:17:07 +0100

    rudimentary api established

--- api/handlers.py
+++ api/handlers.py
@@ -2,13 +2,68 @@ from piston.handler import BaseHandler
 from callamedic.models import Incident
 
 class IncidentHandler(BaseHandler):
-	allowed_methods = ('GET','POST',)
+	allowed_methods = ('GET','POST','PUT')
 	
-	def read(self, request, id=None):
+	def read(self, request, id=None, android_id=None):
 		if id:
 			return Incident.objects.get(pk=id)
 		else:
 			return 0
-			
+		
 	def create(self, request):
-			return '999 created an Incident'
+		data = request.data
+		return data
+		
+	def update(self, request, id=None):
+		data = request.data
+		
+		if data["status"] == "closed":
+			return "Incident %s is closed" % id
+		elif data["status"] == "open":
+			return "Incident %s is open" % id
+		else:
+			return 0
+		
+class ResponderHandler(BaseHandler):
+	allowed_methods = ('POST','PUT',)
+	
+	def create(self, request):
+		data = request.data
+		return "user with the following details has been created: %s" % data
+	
+	def update(self, request, android_id = None):
+		data = request.data
+		
+		if data["on_call"] == "True":
+			response = "%s is logged in" % android_id
+		elif data["on_call"] == "False":
+			response = "%s is logged out" % android_id
+		else:
+			response = 0
+		
+		return response
+		
+class ResponderLocationHandler(BaseHandler):
+	allowed_methods = ('POST',)
+	
+	def create(self, request):
+		data = request.data
+		
+		response = "%s is at location %s , %s" % (data["android_id"],data["lat"],data["lon"])
+		
+		return response
+		
+class IncidentResponderHandler(BaseHandler):
+	allowed_methods = ('PUT',)
+	
+	def update(self, request, incident_id = None, android_id = None):
+		data = request.data
+		
+		response = "%s responders status on incident %s changed to %s" % (android_id, incident_id, data["status"])
+		
+		return response
+			
+		
+		
+		
+		
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from medic.api.handlers import IncidentHandler
+from medic.api.handlers import IncidentHandler, ResponderHandler, ResponderLocationHandler, IncidentResponderHandler
 
 class CsrfExemptResource( Resource ):
     def __init__( self, handler, authentication = None ):
@@ -8,8 +8,17 @@ class CsrfExemptResource( Resource ):
         self.csrf_exempt = getattr( self.handler, 'csrf_exempt', True )
 
 incident_handler = CsrfExemptResource(IncidentHandler)
+responder_handler = CsrfExemptResource(ResponderHandler)
+responderlocation_handler = CsrfExemptResource(ResponderLocationHandler)
+incidentresponder_handler = CsrfExemptResource(IncidentResponderHandler)
+
+
 
 urlpatterns = patterns('',
-    url(r'^incident/(?P<id>[^/]+)/', incident_handler),
-	url(r'^incident/', incident_handler),
+    url(r'^incident/(?P<id>[^/]+)/(?P<android_id>[^/]+)/', incident_handler),
+	url(r'^incident/$', incident_handler),
+	url(r'^responder/(?P<android_id>[^/]+)/', responder_handler),
+	url(r'^responder/$', responder_handler),
+	url(r'^responder_location/$', responderlocation_handler),
+	url(r'^incident_responder/(?P<incident_id>[^/]+)/(?P<android_id>[^/]+)/', incidentresponder_handler),
 )
